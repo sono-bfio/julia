1e45a62e7b7f943d25144a32bd750537e53dfc6d
Yichao Yu <yyc1992@gmail.com>
2016-07-30 08:04:26 +0800

    Add more `try`-`catch` and sigatomic for top-level code/new tasks
    So that we don't need to run `jl_exit` in strange (signal handler) context
    due to missing exception handler.

--- base/task.jl
+++ base/task.jl
@@ -105,6 +105,7 @@ suppress_excp_printing(t::Task) = isa(t.storage, ObjectIdDict) ? get(get_task_tl
 
 # runtime system hook called when a task finishes
 function task_done_hook(t::Task)
+    # `finish_task` sets `sigatomic` before entering this function
     err = (t.state == :failed)
     result = t.result
     handled = false
@@ -152,6 +153,8 @@ function task_done_hook(t::Task)
             end
         end
     end
+    # Clear sigatomic before waiting
+    sigatomic_end()
     wait()
 end
 
